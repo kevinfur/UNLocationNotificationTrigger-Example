4e5744720b4651526c7e299e5c56a37a560b3c31
Kevin Alan Furman <kfurman@despegar.com>
2016-11-22 16:28:20 -0300

    First commit

--- localNotification/ViewController.swift
+++ localNotification/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import UserNotifications
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters // para gastar menos bateria
+        //locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.requestWhenInUseAuthorization()
+        
+        UNUserNotificationCenter.current().delegate = self
+    }
+    
+    @IBAction func RegisterNotification(_ sender: Any) {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func ScheduleNotification(_ sender: Any) {
+        let center = UNUserNotificationCenter.current()
+        
+        center.removeAllPendingNotificationRequests() // borra las notificaciones locales pendientes, hay un limite
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Qué haces acá pibe!?"
+        content.body = "Es peligroso estar por esta zona"
+        content.categoryIdentifier = "alarm"
+        content.sound = UNNotificationSound.default()
+        
+        // Trigger within a timeInterval
+        // let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        //Trigger within a Location
+        let centerLoc = CLLocationCoordinate2D(latitude: -34.6038148, longitude: -58.3792672)
+        let region = CLCircularRegion(center: centerLoc, radius: 2000.0, identifier: UUID().uuidString)
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        let trigger = UNLocationNotificationTrigger(region: region, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
-
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound])
+    }
 
 }
 
